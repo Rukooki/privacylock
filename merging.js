d12d3a1804ff0e7b1d9498b502a0e43c8aec1f6e
Abuelo <kule.wilson@gmail.com>
2026-07-23 21:40:03 +0300

    my code

--- merging.js
+++ merging.js
@@ -5,8 +5,8 @@ console.log('Script started');
 const dbConfig = {
   host: '127.0.0.1',
   port: 3306,
-  user: 'root',
-  password: '',
+  user: 'wilson',
+  password: 'Kule117@33!',
   database: 'cookiego_testing_db'
 };
 
